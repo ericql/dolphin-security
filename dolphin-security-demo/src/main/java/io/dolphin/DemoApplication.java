231f2ea38e35a0dbd98d0ae90108894cb2d9aa57
Eric Liang <120444121@hdjt.hdad.local>
2020-09-26 12:20:04 +0800

    hello Security提交

---
+++ dolphin-security-demo/src/main/java/io/dolphin/DemoApplication.java
@@ -0,0 +1,24 @@
+package io.dolphin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Eric Liang
+ * @Since: 2020-9-26 11:36
+ */
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
