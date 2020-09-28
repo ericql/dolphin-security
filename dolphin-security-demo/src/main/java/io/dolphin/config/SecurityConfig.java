00d76d22926d5257add58f43a9e865cc188ee317
Eric Liang <120444121@hdjt.hdad.local>
2020-09-28 21:41:25 +0800

    去掉登录验证和打jar包设置

---
+++ dolphin-security-demo/src/main/java/io/dolphin/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package io.dolphin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description:
+ * @Author: Eric Liang
+ * @Since: 2020-9-28 21:11
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .anyRequest().permitAll().and().logout().permitAll();//配置不需要登录验证
+    }
+}
